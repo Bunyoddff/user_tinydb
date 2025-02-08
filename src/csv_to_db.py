0bb51d01aa72b69e804efd043fde73d87ddeb095
Bunyoddff <bkhusanov444@gmail.com>
2025-02-08 15:32:49 +0500

    Query and databse

--- src/csv_to_db.py
+++ src/csv_to_db.py
@@ -25,9 +25,13 @@ def insert_into_db(data, db_path=None):
         information.append(db.insert(i))
     return information
 
-def query_db(db_path, query_field, query_value):
+def query_db(db_path, query_field=None, query_value=None):
     # Query the database
-    pass
+    db=TinyDB(db_path)
+    if query_db and query_field:
+        QueryObekti=Query
+        return db.search(QueryObekti[query_field]==query_value)
+    return db.all()
 
 if __name__ == "__main__":
     # Main execution logic
